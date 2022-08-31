cdf42e831c5f4906f9777562ab8c951a2302159c
Cristiano <weydsoncristiano@hotmail.com>
2022-08-31 18:23:29 -0300

    resolução exercicio 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,23 +4,45 @@ import { createUser } from '../services/userAPI';
 import Loading from '../Loading';
 
 class Login extends React.Component {
+  state = {
+    nome: '',
+    loading: false,
+    habilitar: false,
+  };
+
+  handleClick = () => {
+    const { nome } = this.state;
+    this.setState({ habilitar: true });
+    this.setState({ loading: true }, async () => {
+      await createUser({ name: nome });
+      this.setState({ loading: false });
+    });
+  };
+
   render() {
+    const { nome, loading, habilitar } = this.state;
+    const magicNumber = 3;
     return (
       <div data-testid="page-login">
-        login
         <input
           data-testid="login-name-input"
           type="text"
-          name="login"
-          id="login"
-          placeHolder="user"
+          onChange={ ({ target: { value } }) => this.setState({ nome: value }) }
         />
         <button
           data-testid="login-submit-button"
           type="button"
+          onClick={ this.handleClick }
+          disabled={ nome.length < magicNumber }
         >
           Entrar
         </button>
+        {loading && (
+          <Loading />
+        )}
+        {(!loading && habilitar) && (
+          <Redirect to="/search" />
+        )}
       </div>
     );
   }
