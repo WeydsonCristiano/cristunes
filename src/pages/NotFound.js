657db81bbd5a7e8044832104cbbf11f8e60b8ee6
Cristiano <weydsoncristiano@hotmail.com>
2022-08-31 16:46:25 -0300

    resolução exercicio 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        notFound
+      </div>
+    );
+  }
+}
+
+export default NotFound;
