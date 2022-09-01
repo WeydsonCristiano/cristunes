03c62a115b9cacf013782dccd2865ec49f528c1a
Cristiano <weydsoncristiano@hotmail.com>
2022-09-01 18:33:33 -0300

    resolução 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Loading from '../Loading';
@@ -7,29 +8,31 @@ class Search extends React.Component {
   state = {
     loading: false,
     pesquisar: '',
+    resultadoPesquisa: [],
+    nomeArtista: '',
   };
-
-  componentDidMount() {
-    this.pesquisarBotao();
-  }
 
   handleChanger = ({ target: { value } }) => {
     this.setState({ pesquisar: value });
   };
 
   pesquisarBotao = async () => {
-    const { pesquisar, loading } = this.state;
-    this.setState( loading: true );
-    const resultadoPesquisa = await searchAlbumsAPI({ pesquisar });
+    const { pesquisar } = this.state;
     this.setState({
-      pesquisa: resultadoPesquisa,
+      loading: true,
+      pesquisar: '',
+    });
+    const resultadoPesquisa = await searchAlbumsAPI(pesquisar);
+    this.setState({
+      resultadoPesquisa,
       loading: false,
+      nomeArtista: pesquisar,
     });
   };
 
   render() {
     const magicNumber = 2;
-    const { pesquisar } = this.state;
+    const { pesquisar, loading, resultadoPesquisa, nomeArtista } = this.state;
     return (
       <div data-testid="page-search">
         Search
@@ -55,9 +58,37 @@ class Search extends React.Component {
             Pesquisar
           </button>
         </div>
+        <h1>
+          Resultado de álbuns de:
+          { ' ' }
+          {nomeArtista}
+        </h1>
+        <div>
+          { loading && <Loading /> }
+          {loading === false && (
+            resultadoPesquisa < 1 && <p>Nenhum álbum foi encontrado</p>
+          )}
+          {resultadoPesquisa.length > 0 && resultadoPesquisa.map((e, index) => (
+            <Link
+              key={ index }
+              data-testid={ `link-to-album-${e.collectionId}` }
+              to={ `/album/${e.collectionId}` }
+            >
+              <div>
+                <img src={ e.artworkUrl100 } alt={ e.artistName } />
+                {e.collectionName}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
 export default Search;
+// ✕ Será validado se ao clicar em pesquisar, a requisição é feita usando a searchAlbumsAPI (3067 ms)
+// ✕ Será validado se ao clicar no botão, o texto Resultado de álbuns de: <artista> aparece na tela (2646 ms)
+// ✓ Será validado se ao receber o retorno da API, os álbuns são listados na tela (1625 ms)
+// ✕ Será validado se caso a API não retorne nenhum álbum, a mensagem Nenhum álbum foi encontrado é exibida (3014 ms)
+// ✓ Será validado se existe um link para cada álbum listado que redirecione para a rota /album/:id (1665 ms)
--- src/services/searchAlbumsAPI.js
+++ src/services/searchAlbumsAPI.js
@@ -2,6 +2,7 @@ const searchAlbumsAPI = async (artist) => {
   const artistNameURL = encodeURI(artist).replaceAll('%20', '+');
 
   const getAlbumsAPI = `https://itunes.apple.com/search?entity=album&term=${artistNameURL}&attribute=allArtistTerm`;
+  // const cors = `https://itunes.apple.com/search?term=${artistNameURL}&enitity=album`;
 
   const APIResponse = await fetch(getAlbumsAPI);
 
