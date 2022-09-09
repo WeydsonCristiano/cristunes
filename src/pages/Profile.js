c76ccdb0e824a4e377f1df2f8b9e697f72d4538f
Cristiano <weydsoncristiano@hotmail.com>
2022-09-09 11:04:38 -0300

    requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../Loading';
 
 class Profile extends React.Component {
+  state = {
+    loading: true,
+    user: {},
+  };
+
+  componentDidMount() {
+    this.getUsuario();
+  }
+
+  getUsuario = async () => {
+    const usuario = await getUser();
+    this.setState({ user: usuario, loading: false });
+    console.log(usuario);
+  };
+
   render() {
+    const { user, loading } = this.state;
     return (
-      <div data-testid="page-profile">
+      <div>
         <Header />
+        {loading ? <Loading /> : (
+          <>
+            <img
+              data-testid="profile-image"
+              src={ user.image }
+              alt="User"
+            />
+            <p>{ user.name }</p>
+            <p>{ user.email }</p>
+            <p>{ user.description }</p>
+          </>
+        )}
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
